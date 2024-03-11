0b713a702b1b4f0d6b49adfe9d8b20af2ad7b4fa
Alimul-Mahfuz <automation33@mis.prangroup.com>
2024-03-11 12:09:31 +0600

    .

---
+++ AmusingJoke.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AmusingJoke {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String guest=sc.nextLine();
+        String host=sc.nextLine();
+        String pile=sc.nextLine();
+        char[] pileName=pile.toCharArray();
+        char[] guestName=guest.toCharArray();
+        char[] hostName=host.toCharArray();
+        int guestHostLenght=guestName.length+hostName.length;
+        char[] combined=new char[guestHostLenght];
+        System.arraycopy(guestName, 0, combined, 0, guestName.length);
+        System.arraycopy(hostName, 0, combined, guestName.length, hostName.length);
+        Arrays.sort(combined);
+        Arrays.sort(pileName);
+        if(Arrays.equals(combined, pileName)){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+        sc.close();
+
+    }
+}
