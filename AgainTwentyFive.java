74e83db1ff899436bed14ab010b3f5fcf5eafb15
Alimul-Mahfuz <automation33@mis.prangroup.com>
2024-03-12 13:53:08 +0600

    .

--- AgainTwentyFive.java
+++ AgainTwentyFive.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 public class AgainTwentyFive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n = sc.nextDouble();
-        double data = Math.pow(5, n);
-        System.out.println(data);
-        // int[] arr = new int[2];
-        // int i = 0;
-        // while (i < 2) {
-        //     arr[i] = (int)data % 10;
-        //     data = data / 10;
-        //     i++;
-        // }
-        // System.out.printf("%d%d\n",arr[1],arr[0]);
+        sc.nextLong();
+        System.out.println(25);
+        sc.close();
 
     }
 }
