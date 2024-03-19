c4cd67939c93ce4ea0eece377d47154d00babcd0
Alimul-Mahfuz <alimulmahfuztushar@gmail.com>
2024-03-19 18:58:01 +0600

    .

--- IlyaAndBankAccount.java
+++ IlyaAndBankAccount.java
@@ -3,35 +3,43 @@ public class IlyaAndBankAccount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
+        sc.close();
         if(a>0){
             System.out.println(a);
             return;
         }
         int last;
         int beforelast;
-        last=a%10;
+        last=(-1)*a%10;
         beforelast=(-1)*(a/10)%10;
-        String num= Integer.toString(10);
-        System.out.println(num);
+        String num= Integer.toString(Math.abs(a));
         int len=num.length();
-        if(last>beforelast){
-            if(len<2){
-                System.out.println(beforelast);
+        if(len==2){
+            if(last>beforelast){
+                System.out.println(beforelast*(-1));
                 return;
             }
-            int numA=Integer.parseInt(num.substring(0, len-1));
-            System.out.println(numA);
-            return;
-
-        }else{
-            if(len<2){
-                System.out.println(last);
+            else{
+                System.out.println(last*(-1));
                 return;
             }
-            int numB=Integer.parseInt(num.substring(0, len-2));
-            System.out.println(numB);
-            return;
         }
+        else{
+            if(last>beforelast){
+                String numf=num.substring(0, len-1);
+                System.out.println((-1)*Integer.parseInt(numf));
+                return;
+            }
+            else{
+                String num1=num.substring(0, len-2);
+                String num2=num.substring(len-1, len);
+                String numf=num1.concat(num2);
+                System.out.println((-1)*Integer.parseInt(numf));
+                return;
+            }
+        }
+        
+        
 
         
         
