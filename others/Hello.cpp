6b9e819436fe3a13a083810a47aa605b22943068
Alimul-Mahfuz <automation33@mis.prangroup.com>
2024-03-23 10:01:35 +0600

    .

--- others/Hello.cpp
+++ others/Hello.cpp
@@ -1,14 +1,50 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main()
+{
+    int n;
 
-    int arr[]={6,1,2,4,5};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    sort(arr,arr+n);
-    for(int i:arr){
-        cout<<i;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
-    
+
+    for(int i=0;i<n; i++){
+        int sum =0, total=0;
+        int x= arr[i]/2;
+
+        if(x%2!=0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+            for(int j=2; j<=arr[i];j++){
+                if(j%2!=0){
+                    continue;
+                }
+                else{
+                    sum=sum+j;
+                    cout<<j<<" ";
+                }
+            }
+            for(int j=2; j<=arr[i]-1;j++){
+                if(j%2!=0){
+                    continue;
+                }
+                else{
+                    total=total+j-1;
+                    cout<<j-1<<" ";
+                }
+            }
+            for(int k=arr[i]; k<=arr[i]; k++){
+                cout<<sum-total;
+            }
+            cout<<endl;
+        }
+    }
 }
